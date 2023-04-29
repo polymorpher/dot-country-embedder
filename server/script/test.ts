fba484bdd2bdc35264bc2faf3df7355a2ebd3bf2
polymorpher <aaron@qokka.ai>
2023-04-28 22:43:04 -0700

    recursively discover notion subpages

--- server/script/test.ts
+++ server/script/test.ts
@@ -1,8 +1,11 @@
-import { getPage } from '../src/notion.ts'
+import { getPage, getAllPageIds } from '../src/notion.ts'
 
 async function main (): Promise<void> {
-  const ret = await getPage('f9bd34308dc44b32bb23923318da498c')
-  console.log(JSON.stringify(ret))
+  // const ret = await getPage('f9bd34308dc44b32bb23923318da498c')
+  // console.log(JSON.stringify(ret))
+  const ret = await getAllPageIds('ae42787a7d774e3bb86dcd897f720a0b', 2)
+  // const ret = await getAllPageIds('26e40e9d33ab436eab4533c45ccd6d6c', 1)
+  console.log(ret)
 }
 
 main().catch(err => { console.error(err) })
--- server/src/notion.ts
+++ server/src/notion.ts
@@ -2,6 +2,7 @@ import { NotionAPI } from 'notion-client'
 import { type ExtendedRecordMap } from 'notion-types'
 import axios from 'axios'
 import { JSDOM } from 'jsdom'
+import { uniq } from 'lodash-es'
 const notion = new NotionAPI()
 
 const axiosBase = axios.create({ timeout: 15000 })
@@ -37,4 +38,59 @@ export async function getNotionPageId (url: string): Promise<string> {
     return ''
   }
   return pageId
+}
+
+function extractInternalLinksFromProperties (props): string[] {
+  // console.log(props)
+  if (props instanceof Array && props[0] === 'a' && typeof props[1] === 'string') {
+    return [props[1]]
+  }
+  if (props instanceof Array) {
+    return props.filter(e => e instanceof Array || e instanceof Object).flatMap(extractInternalLinksFromProperties)
+  }
+  if (props instanceof Object) {
+    return Object.entries(props).flatMap(([k, v]) => extractInternalLinksFromProperties(v))
+  }
+  return []
+}
+function cleanLink (link: string): string {
+  if (!link) {
+    return ''
+  }
+  if (link.includes('?')) {
+    return link.split('?')[0]
+  }
+  return link.split('#')[0]
+}
+export async function getAllPageIds (id: string, depth: number = 0): Promise<string[]> {
+  let ret: string[] = []
+  let root: ExtendedRecordMap
+  try {
+    root = await getPage(id)
+  } catch (ex) {
+    console.error(`Error retrieving for page id ${id}`, ex)
+    return ret
+  }
+  for (const [, block] of Object.entries(root.block)) {
+    const props = block?.value?.properties
+    if (!props) {
+      continue
+    }
+    const links = extractInternalLinksFromProperties(props)
+    const uniqueLinks = uniq(links.map(cleanLink)).filter(e => e !== `/${id}`)
+    const filteredUniqueLinks = uniqueLinks.filter(e => e.startsWith('/'))
+    if (filteredUniqueLinks.length === 0) {
+      continue
+    }
+    console.log(id, filteredUniqueLinks)
+    ret.push(...filteredUniqueLinks)
+    ret = uniq(ret)
+  }
+  if (depth === 0) {
+    return ret
+  }
+
+  const promises = ret.map(async pageId => await getAllPageIds(pageId.slice(1), depth - 1))
+  const linkGroups = await Promise.all(promises)
+  return [...ret, ...linkGroups.flat()]
 }
