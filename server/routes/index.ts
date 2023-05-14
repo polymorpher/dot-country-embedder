e73b5c96bc5f429f60e3cf70e0444745359f9d67
polymorpher <aaron@qokka.ai>
2023-05-13 21:36:41 -0700

    cache header

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -40,8 +40,14 @@ const abbrv = (s: string | object, len: number = 10): string => {
 }
 const cached = (ttl?: number) => (req: Request, res: Response, next: NextFunction): void => {
   const key = `${req.method}|${req.path}|${JSON.stringify(req.query)}|${JSON.stringify(req.body)}`
+  const keyContentType = key + '|header|content-type'
   const v = cache.get(key)
   if (v) {
+    const contentType = cache.get(keyContentType)
+    if (contentType) {
+      console.log(`Cache header hit key=[${keyContentType}] value=`, contentType)
+      res.header('content-type', contentType as string)
+    }
     console.log(`Cache hit key=[${key}] value=`, abbrv(v), typeof v)
     res.send(v)
     return
@@ -51,6 +57,11 @@ const cached = (ttl?: number) => (req: Request, res: Response, next: NextFunctio
     res.send = (r) => {
       console.log(`Cache set key=[${key}] value=`, abbrv(r), typeof r)
       cache.set(key, r, { ttl: ttl ?? 60 * 1000 })
+      if (res.hasHeader('content-type')) {
+        const h = res.getHeader('content-type')
+        console.log(`Cache header set key=[${keyContentType}] value=`, h)
+        cache.set(keyContentType, h, { ttl: ttl ?? 60 * 1000 })
+      }
       // @ts-expect-error wrapper
       return res.__send(r)
     }
