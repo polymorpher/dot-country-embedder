ce7da7f72a1e2e6d6a8c07aacc893bb2c9aa7017
polymorpher <aaron@qokka.ai>
2023-06-26 00:36:02 -0700

    fix security check for substackDomain

--- server/middlewares/substackDomain.ts
+++ server/middlewares/substackDomain.ts
@@ -13,25 +13,39 @@ const getDomain = (host: string): string => {
   }
 }
 
+function parseUrl (urlStr: string): URL | null {
+  if (!urlStr?.startsWith('https://') && !urlStr?.startsWith('http://')) {
+    return parseUrl(`https://${urlStr}`)
+  }
+  try {
+    const url = new URL(urlStr)
+    if (!url.host.endsWith('.substack.com')) {
+      console.error(`${url} is not with substack.com`)
+      return null
+    }
+    return url
+  } catch (ex: any) {
+    console.error(ex)
+    return null
+  }
+}
+
 const substackDomain = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const host = req.get('host')
-
   if (!host) {
     return
   }
-
   const subdomain = getSubdomain(host)
   const sld = getSld(host)
 
-  const substackDomain = await client.getLandingPage(sld, subdomain)
+  const landing = await client.getLandingPage(sld, subdomain)
 
-  if (!substackDomain.endsWith('.substack.com')) {
-    res.status(401).send('Not substack page')
+  const url = parseUrl(landing)
+  if (!url) {
+    res.status(401).json({ error: 'Not substack page' })
     return
   }
-
-  res.locals.substackDomain = getDomain(substackDomain)
-
+  res.locals.substackDomain = getDomain(url.host)
   next()
 }
 
