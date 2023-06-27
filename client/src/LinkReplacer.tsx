dba1187d9f739f1a0d9e1562eed25eaca8c1e68f
polymorpher <aaron@qokka.ai>
2023-06-27 16:31:45 -0700

    substack link replacer

--- client/src/LinkReplacer.tsx
+++ client/src/LinkReplacer.tsx
@@ -1,11 +1,10 @@
-
 import React, { useEffect, useState } from 'react'
 import { renderToString } from 'react-dom/server'
-import htmlReactParser from 'html-react-parser'
-import { Element as ParserElement } from 'domhandler/lib/node'
+import htmlReactParser, { Element as ParserElement } from 'html-react-parser'
 import { LoadingScreen } from './components/Misc'
+import config from '../config'
 
-interface NotionLinkReplacerConfig {
+export interface NotionLinkReplacerConfig {
   children: JSX.Element
   pageId: string
   allowedPageIds: string[]
@@ -23,6 +22,9 @@ export const NotionLinkReplacer = ({ children, pageId, allowedPageIds = [] }: No
         }
 
         if (node.name === 'a') {
+          if (!node.attribs.href) {
+            return
+          }
           if (node.attribs.href.startsWith('http://') || node.attribs.href.startsWith('https://')) {
             return
           }
@@ -53,7 +55,14 @@ export const NotionLinkReplacer = ({ children, pageId, allowedPageIds = [] }: No
   return element
 }
 
-export const SubstackLinkReplacer = ({ children, pageId, allowedPageIds = [] }: NotionLinkReplacerConfig): JSX.Element => {
+export interface SubstackLinkReplacerConfig {
+  children: JSX.Element
+  sld: string
+  subdomain: string
+  substackHost: string
+}
+
+export const SubstackLinkReplacer = ({ children, sld, subdomain, substackHost }: SubstackLinkReplacerConfig): JSX.Element => {
   const [element, setElement] = useState<JSX.Element>(<LoadingScreen/>)
   useEffect(() => {
     const str = renderToString(children)
@@ -62,34 +71,28 @@ export const SubstackLinkReplacer = ({ children, pageId, allowedPageIds = [] }: 
         if (node.type !== 'tag' || !(node instanceof ParserElement)) {
           return
         }
+        // const replacementHost = `${subdomain}.${sld}.${config.tld}`
+        const replacementHost = `${subdomain}.${sld}.localhost:3100`
 
         if (node.name === 'a') {
-          if (node.attribs.href.startsWith('http://') || node.attribs.href.startsWith('https://')) {
+          const h = node.attribs.href
+          if (!h) {
             return
           }
-          let replaced = false
-          for (const attr of node.attributes) {
-            if (attr.name !== 'href') {
-              continue
-            }
-            if (attr.value.startsWith('/' + pageId)) {
-              // console.log('replacing', attr.value, attr.value.slice(`/${pageId}`.length))
-              node.attribs[attr.name] = attr.value.slice(`/${pageId}`.length)
-              replaced = true
-            } else {
-              const matchedPageIds = allowedPageIds.filter(e => attr.value.startsWith(`/${e}`))
-              if (attr.value.startsWith('/') && matchedPageIds.length === 0) {
-                node.attribs[attr.name] = `https://notion.so/${attr.value.slice(1)}`
-                replaced = true
-              }
-            }
+          if (h.startsWith(`http://${substackHost}`) || h.startsWith(`https://${substackHost}`)) {
+            node.attribs.href = node.attribs.href.replace(substackHost, replacementHost)
+            return node.cloneNode()
           }
-          if (replaced) {
+          if (h.startsWith('https://substack.com/profile')) {
+            node.attribs.href = `https://${sld}.${config.tld}`
             return node.cloneNode()
           }
         }
+        if (node.attribs.class === 'subscribe-widget') {
+          return <iframe src={`https://${substackHost}/embed`} frameBorder="0" scrolling="no"/>
+        }
       }
     }) as JSX.Element)
-  }, [children, pageId, allowedPageIds])
+  }, [children, sld, subdomain, substackHost])
   return element
 }
