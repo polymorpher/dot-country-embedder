b52e8ff6c247dd55a159585d8fc9afeb50c006f2
polymorpher <aaron@qokka.ai>
2024-02-22 23:49:04 -0800

    fix failure case restart-redirect api

--- server/routes/farcast.ts
+++ server/routes/farcast.ts
@@ -18,8 +18,8 @@ const getOriginalHost = (s: string): string => {
 
 router.post('/callback', async (req, res): Promise<any> => {
   const host = req.get('host')
-  console.log('[/farcast/callback]', host, req.headers)
-  console.log('[/farcast/callback] body', JSON.stringify(req.body))
+  // console.log('[/farcast/callback]', host, req.headers)
+  // console.log('[/farcast/callback] body', JSON.stringify(req.body))
   if (!host?.startsWith(config.farcast.postUrlSubdomainPrefix)) {
     return res.status(HttpStatusCode.BadRequest).send(`Invalid host: ${host}`).end()
   }
@@ -27,7 +27,10 @@ router.post('/callback', async (req, res): Promise<any> => {
   let validatedMessage: Message | undefined
   try {
     const frameMessage = Message.decode(Buffer.from(req.body?.trustedData?.messageBytes || '', 'hex'))
+    // console.log('frameMessage', frameMessage)
+    // console.log('client', client)
     const result = await client?.validateMessage(frameMessage)
+    // console.log('result', result)
     if (result?.isOk() && result.value.valid) {
       validatedMessage = result.value.message
     }
@@ -35,21 +38,26 @@ router.post('/callback', async (req, res): Promise<any> => {
     // Also validate the frame url matches the expected url
     const urlBuffer = validatedMessage?.data?.frameActionBody?.url ?? new Uint8Array([])
     const urlString = Buffer.from(urlBuffer).toString('utf-8')
-    if (validatedMessage && !urlString.startsWith(originalHost)) {
+    const url = new URL(urlString)
+    if (validatedMessage && url.host !== originalHost) {
+      console.error('[/callback] bad url', { originalHost, urlHost: url.host })
       return res.status(HttpStatusCode.BadRequest).send(`Invalid frame url: ${urlBuffer}`).end()
     }
   } catch (e) {
     return res.status(HttpStatusCode.BadRequest).send(`Failed to validate message: ${e}`).end()
   }
   // TODO: mint stuff
-  console.log(validatedMessage)
+  console.log('validatedMessage', validatedMessage)
+  // res.send(renderMintFailed(`${req.protocol}://${host}/${config.farcast.postUrlPath}/redirect`)).end()
   res.send(renderMintSuccess()).end()
   // respond a new frame
 })
 
 router.post('/callback/redirect', async (req, res) => {
-  const host = req.get('host')
-  res.redirect(`${req.protocol}://${host}/`)
+  const host = getOriginalHost(req.get('host') ?? '')
+  const target = `https://${host}/`
+  console.log(`[/callback/redirect] Redirecting to ${target}`)
+  res.redirect(target)
 })
 
 router.get('/callback', async (req, res) => {
