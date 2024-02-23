810c7d812caac1fe053607d8ba3645899cf40d3e
polymorpher <aaron@qokka.ai>
2024-02-23 00:38:24 -0800

    fix import

--- server/src/og.ts
+++ server/src/og.ts
@@ -1,8 +1,5 @@
 import ethers from 'ethers'
-import EWSAbi from '../../contract/abi/EWS.json' assert {
-      type: 'json',
-      integrity: 'sha384-ABC123'
-}
+import EWSAbi from '../../contract/abi/EWS.json' assert {type: 'json'}
 import config from '../config.ts'
 import { type DomainInfo, EWSTypes, type OpenGraphData } from './types.ts'
 import { type EWS } from '../../contract/typechain-types'
