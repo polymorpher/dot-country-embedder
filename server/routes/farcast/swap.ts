ab2fb2925b2a4509b19d7c988a28aed82906eb89
jwst9 <johnathonbaw@gmail.com>
2024-03-26 23:56:10 -0600

    add fixed route /swap/image/:text for image generation

--- server/routes/farcast/swap.ts
+++ server/routes/farcast/swap.ts
@@ -1,12 +1,13 @@
 import router from './index.js'
 import { authMessage, getPageSetting } from './middlewares.js'
 import config from '../../config.js'
-import { lookupFid, renderImageResponse } from '../../src/farcaster.js'
+import { lookupFid, parseTextToSvg, renderImageResponse, svgToPng } from '../../src/farcaster.js'
 import type { Request } from 'express'
+import { HttpStatusCode } from 'axios'
 
 const generateImageResponse = (text: string, req: Request, retry = false): string => {
   const host = req.get('host')
-  const image = `${req.protocol}://${host}/${config.farcast.apiBase}/text/image?t=${encodeURIComponent(text)}`
+  const image = `${req.protocol}://${host}/${config.farcast.apiBase}/swap/image/${encodeURIComponent(text)}`
   const restartTarget = `${req.protocol}://${host}/${config.farcast.apiBase}/callback/redirect`
 
   if (retry) {
@@ -15,6 +16,24 @@ const generateImageResponse = (text: string, req: Request, retry = false): strin
 
   return renderImageResponse(image)
 }
+
+router.get('/swap/image/:text', async (req, res) => {
+  const style = JSON.parse((req.query.s ?? '{}') as string)
+  if (!req.params.text) {
+    return res.status(HttpStatusCode.BadRequest).send('No text provided').end()
+  }
+  const data = parseTextToSvg(req.params.text, style)
+  if (req.query.png) {
+    res.type('png')
+    const png = await svgToPng(data)
+    res.send(png).end()
+    return
+  }
+  res.type('svg')
+  res.header('Cache-Control', 'public, max-age=0, must-revalidate')
+  res.header('Age', '0')
+  res.send(data).end()
+})
 
 router.post('/swap/buy/callback', authMessage, getPageSetting, async (req, res) => {
   if (!req.validatedMessage) {
